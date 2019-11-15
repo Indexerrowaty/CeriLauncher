12d895373c2fb4b2532257cb5a09102cb6d29719
wellls <wellschuan@gmail.com>
2019-11-15 17:05:33 +0800

    Create simplified chinese translation

---
+++ translations/simplified_chinese.dart
@@ -0,0 +1,501 @@
+// LANGUAGE TRANSLATION TEMPLATE
+// Includes all text within Ceri Launcher as of 1.8.0-beta
+
+String translate(text) {
+  switch (text) {
+    //
+    // ----- LOADING TEXT -----
+    //
+
+    case "getting app info":
+      return "正在获取应用信息";
+    case "loading page":
+      return "正在载入界面";
+    case "loading preferences":
+      return "正在载入首选项";
+    case "loading settings":
+      return "正在载入设置项";
+
+    //
+    // ----- SETUP -----
+    //
+
+    case "欢迎来到":
+      return ;
+    case "Ceri Launcher":
+      return "Ceri 启动器";
+
+    case "Swipe left to start":
+      return "向左滑动即可开始";
+
+    case "Select language":
+      return "选择语言";
+
+    case "All languages (other than English) are translated by the community. Some translations may be incorrect or missing.":
+      return "所有语言（除了英语）均由社区提供翻译，如有不妥之处，尽请谅解。";
+
+    case "More languages coming soon":
+      return "将会支持更多语言";
+
+    case "Choose your style":
+      return "选择你的风格";
+
+    case "Choose style":
+      return "选择风格";
+
+    case "All apps":
+      return "全部应用";
+
+    case "Only favorites":
+      return "仅喜欢的应用";
+
+    case "Tray":
+      return "托盘模式";
+
+    case "You can always change this later":
+      return "您以后可以随时更改";
+
+    case "Please choose an option to continue":
+      return "请选择其中一个选项以继续";
+
+    case "Choose your layout":
+      return "选择您的布局";
+
+    case "Choose layout":
+      return "选择布局";
+
+    case "Grid":
+      return "格线";
+
+    case "Honeycomb":
+      return "蜂巢";
+
+    case "Choose your Ceri widget":
+      return "选择您的 Ceri 挂件";
+
+    case "Choose Ceri widget":
+      return "选择 Ceri 挂件";
+
+    case "None":
+      return "无";
+
+    case "Light analog":
+      return "明亮模拟时钟";
+
+    case "Dark analog":
+      return "黑暗模拟时钟";
+
+    case "Digital":
+      return "电子时钟";
+
+    case "Assistant":
+      return "助手时钟";
+
+    case "User widget":
+      return "自定义时钟";
+
+    case "Choose your package":
+      return "选择你的包";
+
+    case "I'm an 18-year-old UK maths student trying to save money to study Cyber Security at university.":
+      return "我是一名 18 岁的数学系学生，我来自英国，我正在攒钱以便在上大学期间能够学习网络安全。";
+    case "If you want access to all features and you don't mind sparing £2, I would be incredibly grateful!":
+      return "如果您想要体验所有的功能，并且您不介意花费两英镑的话，我将感激不尽！";
+
+    case "Upgrade to pro":
+      return "升级至专业版";
+    case "Support me and access all current and future features!":
+      return "支持我并获取所有当前以及未来的功能！";
+
+    case "Restore purchase":
+      return "恢复购买";
+    case "Already supported the project?":
+      return "已经支持过此项目？";
+
+    case "Stay with free version":
+      return "继续使用免费版本";
+    case "Core functionality will always be free":
+      return "核心功能将会继续保持免费";
+
+    case "One last thing":
+      return "还有一件事";
+
+    case "I rely completely on user feedback to help make Ceri Launcher better.":
+      return "我十分依赖用户反馈，以助我将 Ceri 启动器做得更好。";
+    case "Please consider joining":
+      return "请考虑加入";
+    case "the Ceri Launcher subreddit":
+      return "Ceri 启动器的 reddit 版块";
+    case "or sending me an email with your thoughts.":
+      return "或者将您的想法通过邮件发送给我。";
+    case "I'd appreciate it a lot!":
+      return "我会十分感激！";
+    case "Hope you enjoy,":
+      return "希望你能够喜欢,";
+
+    case "Tap anywhere":
+      return "点击任意地方";
+    case "to see your new home":
+      return "查看您的新主页";
+
+    //
+    // ----- HOMESCREEN -----
+    //
+
+    case "Swipe down to search":
+      return "下滑搜索";
+
+    case "Pull down to search":
+      return "下拉搜索";
+
+    case "Long-press an app for options":
+      return "长按一个应用进行更多操作";
+
+    case "Close":
+      return "关闭";
+
+    case "Opens your Ceri Launcher settings":
+      return "打开您的 Ceri 启动器设置项";
+
+    case "Ceri Launcher Settings":
+      return "设置 Ceri 启动器";
+
+    case "Add to favorites":
+      return "添加至我喜欢的应用";
+
+    case "Remove from favorites":
+      return "从我喜欢的应用中删除";
+
+    case "Add back to homescreen":
+      return "添加回主屏幕";
+
+    case "Hide from homescreen":
+      return "从主屏幕隐藏";
+
+    case "Application info":
+      return "应用信息";
+
+    case "Uninstall":
+      return "卸载";
+
+    //
+    // ----- SEARCH -----
+    //
+
+    case "Search the web for":
+      return "从互联网搜索";
+
+    case "Search engine:":
+      return "搜索引擎:";
+
+    //
+    // ----- SETTINGS -----
+    //
+
+    case "Set as home":
+      return "设为默认启动器";
+    case "Open default apps menu":
+      return "打开默认应用菜单";
+
+    case "Device settings":
+      return "系统设置";
+    case "Open the standard settings app":
+      return "打开系统设置项";
+
+    case "Style":
+      return "风格";
+    case "Choose style (All apps, Only favorites, Tray), toggle app dimming":
+      return "选择风格（全部应用，仅喜欢的应用，托盘模式），切换应用亮度";
+
+    case "Wallpapers":
+      return "壁纸";
+    case "Choose wallpaper, apply dim/vignette":
+      return "选择壁纸，选择渐变或者黑暗风格";
+
+    case "Dark mode":
+      return "黑暗模式";
+    case "True dark mode for OLED devices\n(or #000000 lovers:":
+      return "Amoled 黑暗模式";
+
+    case "Configure layout":
+      return "配置布局";
+    case "Standard grid or honeycomb, 2, 3 or 4 columns, customise icon size and padding":
+      return "在标准网格、蜂巢中选择显示2-4列，自定义图标大小和间距";
+
+    case "Icon style":
+      return "图标风格";
+    case "Icon packs, shape, zoom/crop, shadows":
+      return "图标包，形状，大小，阴影等等";
+
+    case "Search options":
+      return "搜索选项";
+    case "Search icon size, search engine":
+      return "搜索图标大小，搜索引擎";
+
+    case "Configure Ceri widget":
+      return "配置 Ceri 挂件";
+    case "Choose Ceri widget, configure padding":
+      return "选择 Ceri 挂件，配置间距";
+
+    case "Scrolling animations":
+      return "滚动动画";
+    case "None, bubble, straight, inwards, outwards, and more":
+      return "无，气泡，直线，向内，向外，以及更多";
+
+    case "Notification dots":
+      return "通知计数";
+    case "See indications on your homescreen for pending notifications":
+      return "在您的主屏幕查看未读通知计数";
+
+    case "Gestures":
+      return "手势";
+    case "Launch apps and actions quickly with side gestures":
+      return "通过边缘手势快速启动应用或者触发动作";
+
+    case "Miscellaneous settings":
+      return "杂项设置";
+    case "Warning: Some of these may not have been fully tested":
+      return "警告：其中部分选项可能未经完全测试通过";
+
+    case "Performance options":
+      return "性能选项";
+    case "Experimental settings that may improve performance":
+      return "可能会提高性能的实验性选项";
+
+    case "About Ceri Launcher":
+      return text;
+    case "Version number, developer info":
+      return text;
+
+    case "Check for updates":
+      return text;
+    case "Check for Ceri Launcher updates on the Play Store":
+      return text;
+
+    case "Support and feedback":
+      return text;
+    case "Go to the Ceri Launcher subreddit":
+      return text;
+
+    case "Email developer":
+      return "发送邮件给开发者";
+    case "Have an issue or suggestion? Really enjoying Ceri Launcher? I try to reply within a few hours ::":
+      return text;
+
+    case "Change language":
+      return text;
+    case "Change the language for the Ceri Launcher interface":
+      return text;
+
+    case "Add or improve translations":
+      return text;
+    case "Use your language skills to help make Ceri Launcher better. These translations will be added periodically in future updates.":
+      return text;
+
+    case "Open source licenses":
+      return text;
+
+    case "Version":
+      return "版本"; // context: "Version 1.7.5-beta" etc.
+    case "Paid — thanks!":
+      return "已付款-感谢！";
+    case "Free version":
+      return text;
+    case "Your own":
+      return text;
+
+    case "Ceri Wallpapers":
+      return text;
+    case "Android Home Xperience":
+      return text;
+    case "Tiles below 'Your own' will open the respective wallpaper album in your browser.":
+      return text;
+    case "Vignette/dim":
+      return text;
+
+    case "Dark mode":
+      return text;
+    case "Standard":
+      return text;
+    case "Black (OLED:":
+      return text;
+    case "Automatic":
+      return text;
+    case "Dark mode off":
+      return text;
+    case "Dark mode on":
+      return text;
+    case "Dark mode enables at night":
+      return text;
+
+    case "Miscellaneous":
+      return text;
+    case "Disable haptics":
+      return text;
+    case "Show package names in search":
+      return text;
+    case "Disable vignette":
+      return text;
+    case "Disable scrolling beyond screen height":
+      return text;
+
+    case "2 columns":
+      return text;
+    case "3 columns":
+      return text;
+    case "Icon size":
+      return text;
+    case "Icon padding":
+      return text;
+    case "Standard grid layout":
+      return text;
+    case "Honeycomb layout":
+      return text;
+    case "Favorites stay at the top":
+      return text;
+    case "Just your favorites, nothing else":
+      return text;
+    case "Automatically show your most used apps only":
+      return text;
+    case "(experimental:":
+      return text;
+    case "Dim apps that aren't in favorites":
+      return text;
+
+    case "Nothing set":
+      return text;
+    case "Select package":
+      return text;
+    case "Actions will be launchable with gestures in an upcoming update.":
+      return text;
+    case "Search":
+      return text;
+
+    case "No icon packs found.":
+      return text;
+    case "None (default:":
+      return text;
+    case "Select icon pack":
+      return text;
+    case "If you have an icon pack installed, choose it here":
+      return text;
+
+    case "Corner radius":
+      return text;
+    case "Zoom/crop":
+      return text;
+    case "Shadows (off:":
+      return text;
+    case "Shadows (subtle:":
+      return text;
+    case "Shadows (heavy:":
+      return text;
+    case "Shadows (heavier:":
+      return text;
+
+    case "App icon size":
+      return text;
+
+    case "Animation strength":
+      return text;
+    case "disabled":
+      return text;
+    case "weak":
+      return text;
+    case "normal":
+      return text;
+    case "strong":
+      return text;
+    case "strongest":
+      return text;
+
+    case "These are EXPERIMENTAL settings to try out if you suffer from poor performance or extreme battery drain when using Ceri Launcher.":
+      return text;
+    case "If you are not having issues with performance it is recommended that you leave these at their defaults.":
+      return text;
+    case "Icon render size":
+      return text;
+    case "Default is 192. Smaller values should result in lower RAM usage (at the expense of image quality:.":
+      return text;
+    case "EXAMPLE":
+      return text;
+    case "Remove icons out of frame":
+      return text;
+    case "Disable antialiasing":
+      return text;
+    case "Force icon caching":
+      return text;
+
+    case "Ceri Launcher utilizes the works of these people:":
+      return text;
+
+    case "EXPERIMENTAL.":
+      return text;
+
+    case "Nothing":
+      return text;
+    case "Light analog clock widget":
+      return text;
+    case "by NPKompleet, The CS Guy and Ali Sayed":
+      return text;
+    case "Dark analog clock widget":
+      return text;
+    case "Digital clock":
+      return text;
+
+    //
+    // ----- WIDGETS -----
+    //
+
+    case "Monday":
+      return text;
+    case "Tuesday":
+      return text;
+    case "Wednesday":
+      return text;
+    case "Thursday":
+      return text;
+    case "Friday":
+      return text;
+    case "Saturday":
+      return text;
+    case "Sunday":
+      return text;
+
+    case "January":
+      return text;
+    case "February":
+      return text;
+    case "March":
+      return text;
+    case "April":
+      return text;
+    case "May":
+      return text;
+    case "June":
+      return text;
+    case "July":
+      return text;
+    case "August":
+      return text;
+    case "September":
+      return text;
+    case "October":
+      return text;
+    case "November":
+      return text;
+    case "December":
+      return text;
+
+    case "Good morning":
+      return text;
+    case "Good afternoon":
+      return text;
+    case "Good evening":
+      return text;
+    case "Sleep well":
+      return text;
+  }
+
+  return text; // return original text if no translation found
+}
